176ee96c2ddb13f09398065522dd682c1eff0ce9
Sahil Sharma <ss573@snu.edu.in>
2017-11-13 22:32:22 +0530

    Init Admin Panel

---
+++ application/controllers/Auth.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function signupView()
+    {
+
+    }
+
+    public function signup()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function loginView()
+    {
+
+    }
+}
